fba786bcc27c85f00b72cb55dca776afb2f8d6ce
ArianeExtern <arianeextern@gmail.com>
2016-07-28 23:26:21 +0000

    Commit Inscription

--- Inscription/resources/views/pages/register.blade.php
+++ Inscription/resources/views/pages/register.blade.php
@@ -4,7 +4,7 @@
 
 <div class="panel panel-info">
 	  <div class="panel-heading">
-			<h3 class="panel-title text-center"><b>S'enregistrer</b></h3>
+			<h3 class="panel-title text-center"><b>S'enregistrer !</b></h3>
 	  </div>
 	  <div class="panel-body">
         <div class="login">
